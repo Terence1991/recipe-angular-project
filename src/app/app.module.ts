7b3e27ea95b72bc9b884ba0a9a717ae11eefaeab
terence1991 <cplterencehawes@gmail.com>
2019-04-12 14:26:55 -0400

    recpie item compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipieDetailComponent } from './recipes/recipie-detail/recipie-detail.component'
+import { RecipieDetailComponent } from './recipes/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './recipes/recipie-list/recipie-item/recipie-item.component'
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent, RecipesComponent, RecipeListComponent, RecipieDetailComponent
+    HeaderComponent, RecipesComponent, RecipeListComponent, RecipieDetailComponent, RecipieItemComponent
   ],
   imports: [
     BrowserModule,
