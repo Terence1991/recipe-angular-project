1a20dd431af2c8a4eb317a8fe78f632dcf5d3630
terence1991 <cplterencehawes@gmail.com>
2019-04-15 13:33:46 -0400

    bug fixed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingrediant } from '../shared/ingrediants.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingrediants = []
+  ingrediants: Ingrediant[] = [
+    new Ingrediant("Gravey", 7),
+    new Ingrediant("Chesse", 9)
+  ];
 
   constructor() { }
 
