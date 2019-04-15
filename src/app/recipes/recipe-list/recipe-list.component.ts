db7ca45a3876b01b292088fdec0e0b943b81e781
terence1991 <cplterencehawes@gmail.com>
2019-04-15 10:31:27 -0400

    html added for list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("poutine", 'french fries and Gravey', "https://banner2.kisspng.com/20180405/xfq/kisspng-poutine-cuisine-of-quebec-french-fries-canadian-cu-french-fries-5ac5ff5fe5d254.4287826115229254079414.jpg")
+
+  ];
 
   constructor() { }
 
